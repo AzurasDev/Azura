39bc709e2eb01b265e5e5726f35351dd3205b80a
Tolde <117697918+ToldeHub@users.noreply.github.com>
2023-02-05 17:40:51 +1100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,16 @@
+local http_request = syn and syn.request or request;
+local hwidPaste = loadstring(game:HttpGet("https://toldehub.github.io/Azura/Keys.lua"))()
 
+
+local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
+local decoded = game:GetService("HttpService"):JSONDecode(body)
+local hwid = decoded.headers['Syn-Fingerprint']
+
+for i, v in pairs(hwidPaste) do
+ if v == hwid then
+    loadstring(game:HttpGet("https://toldehub.github.io/Azura/Script.lua"))()
+  else
+    setclipboard(hwid)
+  })
+  end
+end
